86d60b2149c01ab4613a72e87de073b3c2a58051
Sam Duckett <67984574+samduckett@users.noreply.github.com>
2025-03-21 15:11:06 -0400

    running pollygon

--- Lab 1/main.py
+++ Lab 1/main.py
@@ -1,7 +1,143 @@
-# basic Hello world program
-# documentation https://www.robotmesh.com/studio/content/docs/vexv5-python/html/annotated.html
+# region VEXcode Generated Robot Configuration
 from vex import *
-from RBEDrivetrain import *
+import urandom
+
+# Brain should be defined by default
+brain = Brain()
+
+# Robot configuration code
+
+
+# wait for rotation sensor to fully initialize
+wait(30, MSEC)
+
+
+# Make random actually random
+def initializeRandomSeed():
+    wait(100, MSEC)
+    random = (
+        brain.battery.voltage(MV)
+        + brain.battery.current(CurrentUnits.AMP) * 100
+        + brain.timer.system_high_res()
+    )
+    urandom.seed(int(random))
+
+
+# Set random seed
+initializeRandomSeed()
+
+
+def play_vexcode_sound(sound_name):
+    # Helper to make playing sounds from the V5 in VEXcode easier and
+    # keeps the code cleaner by making it clear what is happening.
+    print("VEXPlaySound:" + sound_name)
+    wait(5, MSEC)
+
+
+# add a small delay to make sure we don't print in the middle of the REPL header
+wait(200, MSEC)
+# clear the console to make sure we don't have the REPL in the console
+print("\033[2J")
+
+# endregion VEXcode Generated Robot Configuration
+from vex import *
+
+
+class RBEDrivetrain:
+    """drivetrain class for RBE 1001, will build on the entire class"""
+
+    def __init__(
+        self,
+        LeftMotor: Motor,
+        RightMotor: Motor,
+        GearRatio: float = 1,
+        wheelDiameter: float = 1,
+        TrackWidth: float = 1,
+        Wheelbase: float = 1,
+    ):
+
+        self.LeftMotor = LeftMotor
+        self.RightMotor = RightMotor
+
+        self.gearRatio = GearRatio
+        """the gear ratio on the drive motor"""
+
+        self.wheelDiameter = wheelDiameter
+        """the radius of the drive wheels"""
+
+        self.wheelCircumference = wheelDiameter * 3.141592
+        """how for the robot will move with 1 rotation"""
+
+        self.trackWidth = TrackWidth
+        """distance from left Wheel to right wheel"""
+
+        self.wheelbase = Wheelbase
+        """distance from front to back wheel"""
+
+        self.rotationsPerInch = 1 / self.wheelCircumference
+        """how Rotations will move the robot 1 inch"""
+
+    def driveStraight(self, Inches: float):
+        """Function to drive BaseBot straight for some number of inches"""
+        Velocity = 100
+        self.LeftMotor.spin_for(
+            FORWARD,
+            Inches * self.gearRatio * self.rotationsPerInch,
+            RotationUnits.REV,
+            Velocity,
+            RPM,
+            False,
+        )
+        self.RightMotor.spin_for(
+            FORWARD,
+            Inches * self.gearRatio * self.rotationsPerInch,
+            RotationUnits.REV,
+            Velocity,
+            RPM,
+            True,
+        )
+
+    def turnInPlace(self, Rotations: float):
+        """Function to turn BaseBot for some number of Rotations"""
+        Velocity = 100
+        self.LeftMotor.spin_for(
+            REVERSE,
+            Rotations * self.gearRatio * self.trackWidth / self.wheelDiameter,
+            RotationUnits.REV,
+            Velocity,
+            RPM,
+            False,
+        )
+        self.RightMotor.spin_for(
+            FORWARD,
+            Rotations * self.gearRatio * self.trackWidth / self.wheelDiameter,
+            RotationUnits.REV,
+            Velocity,
+            RPM,
+            True,
+        )
+
+    def turnAroundWheel(self, Rotations: float):
+        Velocity = 100
+        if Rotations > 0:
+            self.LeftMotor.spin_for(
+                FORWARD,
+                Rotations * self.gearRatio * self.trackWidth / self.wheelDiameter * 2,
+                RotationUnits.REV,
+                Velocity,
+                RPM,
+                True,
+            )
+        else:
+            self.RightMotor.spin_for(
+                REVERSE,
+                Rotations * self.gearRatio * self.trackWidth / self.wheelDiameter * 2,
+                RotationUnits.REV,
+                Velocity,
+                RPM,
+                True,
+            )
+
 
 # initialize the brian
 brain = Brain()
@@ -39,4 +175,4 @@ def maze():
     drivetrain.driveStraight(5.5)
 
 
-maze()
+polygon(3, 10.0)
