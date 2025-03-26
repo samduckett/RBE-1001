a287a6bae08a19e6ebccdab90305ef262db942fa
Sam Duckett <67984574+samduckett@users.noreply.github.com>
2025-03-26 11:29:29 -0400

    adding pre lab to drivetrain class

--- Lab 2/Drivetrin.py
+++ Lab 2/Drivetrin.py
@@ -14,8 +14,8 @@ class RBEDrivetrain:
         Wheelbase: float = 1,
     ):
 
-        self.LeftMotor = LeftMotor
-        self.RightMotor = RightMotor
+        self.leftMotor = LeftMotor
+        self.rightMotor = RightMotor
 
         self.gearRatio = GearRatio
         """the gear ratio on the drive motor"""
@@ -35,10 +35,30 @@ class RBEDrivetrain:
         self.rotationsPerInch = 1 / self.wheelCircumference
         """how Rotations will move the robot 1 inch"""
 
+    def configPIDWallFollow(
+        self, K_P, wallFollowSpeed, frontRangeFinder, rightRangeFInder
+    ):
+        # 51
+        # 10 3/4
+        self.K_P = K_P
+        self.wallFollowSpeed = wallFollowSpeed
+        self.frontRangeFinder = frontRangeFinder
+        self.rightRangeFInder = rightRangeFInder
+
+    def drive(self, speed, direction):
+        """drive function - For negative values of direction, the robot turns right, and for positive values of direction, the robot turns left.  For values of direction with small magnitudes, the robot gradually turns.  For values of direction with large magnitudes, the robot turns more quickly."""
+        self.leftMotor.spin(FORWARD, speed * direction, RPM)
+        self.rightMotor.spin(FORWARD, speed * direction, RPM)
+
+    def wallFollowInches(self, setDistanceFromWall):
+        while self.frontRangeFinder.distance(DistanceUnits.IN) != setDistanceFromWall:
+            rightError = setDistanceFromWall - self.frontRangeFinder.distance()
+            self.drive(setDistanceFromWall, -self.K_P * rightError)
+
     def driveStraight(self, Inches: float):
         """Function to drive BaseBot straight for some number of inches"""
         Velocity = 100
-        self.LeftMotor.spin_for(
+        self.leftMotor.spin_for(
             FORWARD,
             Inches * self.gearRatio * self.rotationsPerInch,
             RotationUnits.REV,
@@ -46,7 +66,7 @@ class RBEDrivetrain:
             RPM,
             False,
         )
-        self.RightMotor.spin_for(
+        self.rightMotor.spin_for(
             FORWARD,
             Inches * self.gearRatio * self.rotationsPerInch,
             RotationUnits.REV,
@@ -58,7 +78,7 @@ class RBEDrivetrain:
     def turnInPlace(self, Rotations: float):
         """Function to turn BaseBot for some number of Rotations"""
         Velocity = 100
-        self.LeftMotor.spin_for(
+        self.leftMotor.spin_for(
             REVERSE,
             Rotations * self.gearRatio * self.trackWidth / self.wheelDiameter,
             RotationUnits.REV,
@@ -66,7 +86,7 @@ class RBEDrivetrain:
             RPM,
             False,
         )
-        self.RightMotor.spin_for(
+        self.rightMotor.spin_for(
             FORWARD,
             Rotations * self.gearRatio * self.trackWidth / self.wheelDiameter,
             RotationUnits.REV,
@@ -78,7 +98,7 @@ class RBEDrivetrain:
     def turnAroundWheel(self, Rotations: float):
         Velocity = 100
         if Rotations > 0:
-            self.LeftMotor.spin_for(
+            self.leftMotor.spin_for(
                 FORWARD,
                 Rotations * self.gearRatio * self.trackWidth / self.wheelDiameter * 2,
                 RotationUnits.REV,
@@ -87,7 +107,7 @@ class RBEDrivetrain:
                 True,
             )
         else:
-            self.RightMotor.spin_for(
+            self.rightMotor.spin_for(
                 REVERSE,
                 Rotations * self.gearRatio * self.trackWidth / self.wheelDiameter * 2,
                 RotationUnits.REV,
@@ -95,73 +115,3 @@ class RBEDrivetrain:
                 RPM,
                 True,
             )
-
-
-# ---------------------------------------------------------------------------- #
-#                                                                              #
-# 	Module:       main.py                                                      #
-# 	Author:       nbertozzi                                                    #
-# 	Created:      10/19/2023, 6:27:34 PM                                       #
-# 	Description:  V5 project                                                   #
-#                                                                              #
-# ---------------------------------------------------------------------------- #
-
-# Library imports
-from vex import *
-
-# Brain should be defined by default
-brain=Brain()
-
-left_motor = Motor(Ports.PORT1, 18_1, True)
-right_motor = Motor(Ports.PORT10, 18_1, False)
-rangeFinderFront = Sonar(brain.three_wire_port.g)
-rangeFinderRightSide = Sonar(brain.three_wire_port.a)
-
-WHEEL_DIAMETER = 4.0
-GEAR_RATIO = 5.0
-WHEEL_TRACK = 11.0 
-
-K_P = 10
-SET_WALL_FOLLOW_SPEED = 70         # RPM
-SET_WALL_DISTANCE = 11            # inches for wall following
-TURN_ANGLE_AT_WALL = 90           # turn left angle in degrees
-SET_DISTANCE_TO_START_TURN = 8      # inches from wall in front to begin turn
-
-# drive function - For negative values of direction, the robot turns right, and for positive
-# values of direction, the robot turns left.  For values of direction with small magnitudes, the
-# robot gradually turns.  For values of direction with large magnitudes, the robot turns more
-# quickly.
-
-def drive(speed, direction):
-   left_motor.set_velocity(speed * direction, RPM)
-   right_motor.set_velocity(speed * direction, RPM)
-   left_motor.spin(FORWARD)
-   right_motor.spin(FORWARD)
-
-# Function to wall follow at set distance from wall
-
-def wallFollowInches(setDistanceFromWall):
-    rightError = setDistanceFromWall - rangeFinderFront.distance()
-    drive(SET_WALL_FOLLOW_SPEED, -K_P*rightError)
-
-# Function to turn BaseBot for some number of degrees
-
-def turnInPlace(robotTurnInDegrees):
-    left_motor.spin_for(REVERSE, robotTurnInDegrees*GEAR_RATIO*WHEEL_TRACK/WHEEL_DIAMETER, DEGREES, 100, RPM, False)
-    right_motor.spin_for(FORWARD, robotTurnInDegrees*GEAR_RATIO*WHEEL_TRACK/WHEEL_DIAMETER, DEGREES, 100, RPM, True)
-
-      
-# Program to follow wall, turn left at next wall, and follow wall indefinitely
-
-rangeFinderFront.distance(DistanceUnits.IN)     # acquire initial distance values
-rangeFinderRightSide.distance(DistanceUnits.IN)
-
-wait(500)
-
-while(rangeFinderFront.distance(DistanceUnits.IN) != SET_WALL_DISTANCE):
-    wallFollowInches(SET_WALL_DISTANCE)
-
-turnInPlace(TURN_ANGLE_AT_WALL)
-
-while(True):
-    wallFollowInches(SET_WALL_DISTANCE)
--- Lab 2/Main.py
+++ Lab 2/Main.py
@@ -2,3 +2,25 @@ from vex import *
 from Drivetrin import *
 
 # TEST COMMTI FROM BISHOP
+
+
+# initialize the brian
+brain = Brain()
+
+# initialize the left and right motor
+left_motor = Motor(Ports.PORT1, 18_1, False)
+right_motor = Motor(Ports.PORT10, 18_1, True)
+
+frontRangeFinder = Sonar(brain.three_wire_port.g)
+rightRangeFInder = Sonar(brain.three_wire_port.a)
+
+frontRangeFinder.distance(DistanceUnits.IN)  # acquire initial distance values
+rightRangeFInder.distance(DistanceUnits.IN)
+
+wait(500)
+
+brain.screen.print("Hello, World")
+
+drivetrain = RBEDrivetrain(left_motor, right_motor, 5, 4, 11.0)
+
+drivetrain.configPIDWallFollow(10, 100, frontRangeFinder, rightRangeFInder)
