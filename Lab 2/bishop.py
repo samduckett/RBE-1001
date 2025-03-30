74f798b68046bf2624aac2a383f82c9d42d703fd
bishopgov <brutalsysten.mail@gmail.com>
2025-03-30 15:24:59 -0400

    Fixed Merge

--- Lab 2/bishop.py
+++ Lab 2/bishop.py
@@ -11,6 +11,7 @@
 # from vex import *
 
 
+
 # # Brain should be defined by default
 # brain=Brain()
 # leftMotor = Motor(Ports.PORT1, 18_1, False)
@@ -28,39 +29,16 @@
 #     wait(500)
 
 
+
 from vex import *
 
 
-class arm:
-    pass
+
+
+
 
 
 class RBEDrivetrain:
-    """drivetrain class for RBE 1001, will build on the entire class"""
-
-    def __init__(
-        self,
-        frontRangeFinder: Sonar,
-        rightRangeFinder: Sonar,
-        motorLeft: Motor,
-        motorRight: Motor,
-        finalDrive: float,
-        wheelDiameter: float,
-        wheelBase: float,
-        wheelTrack: float,
-    ):
-        # 51
-        # 10 3/4
-        self.frontRangeFinder = frontRangeFinder
-        self.rightRangeFinder = rightRangeFinder
-        self.motorLeft = motorLeft
-        self.motorRight = motorRight
-        self.finalDrive = finalDrive
-        self.wheelDiameter = wheelDiameter
-        self.wheelBase = wheelBase
-        self.wheelTrack = wheelTrack
-        self.wheelCircumference = self.wheelDiameter * math.pi
-        self.rotationsPerInch = 1 / self.wheelCircumference
 	"""drivetrain class for RBE 1001, will build on the entire class"""
 	def __init__(
 		self,
@@ -87,42 +65,29 @@ class RBEDrivetrain:
 		self.rotationsPerInch = 1 / self.wheelCircumference
 		self.wheelRotDegPerBodyTurnDeg = ((wheelTrack * 2) / wheelDiameter)
 
-    def driveLeftMotor(self, speed):
-        """drive function - For negative values of direction, the robot turns right, and for positive values of direction, the robot turns left.  For values of direction with small magnitudes, the robot gradually turns.  For values of direction with large magnitudes, the robot turns more quickly."""
-        self.motorLeft.set_velocity(speed, RPM)
-        self.motorLeft.spin(FORWARD)
+		
 
-    def driveRightMotor(self, speed):
-        self.motorRight.set_velocity(speed, RPM)
-        self.motorRight.spin(FORWARD)
 
-    def brazeWallUntilDistance(self, rightFollowDist, forwardDistWall, speed, kp):
-        while self.frontRangeFinder.distance(DistanceUnits.IN) >= forwardDistWall:
-            rightError = rightFollowDist - self.rightRangeFinder.distance(
-                DistanceUnits.IN
-            )
-            brain.screen.print_at(
-                "FRONT SENSOR", rangeFinderFront.distance(DistanceUnits.IN), x=40, y=40
-            )
-            brain.screen.print_at(
-                "RIGHT SENSOR", rangeFinderRight.distance(DistanceUnits.IN), x=40, y=90
-            )
-            self.driveRightMotor(speed)
-            self.driveLeftMotor(kp * rightError + speed)
-        self.motorLeft.stop(HOLD)
-        self.motorRight.stop(HOLD)
+	def driveLeftMotor(self, speed):
+		"""drive function - For negative values of direction, the robot turns right, and for positive values of direction, the robot turns left.  For values of direction with small magnitudes, the robot gradually turns.  For values of direction with large magnitudes, the robot turns more quickly."""
+		self.motorLeft.set_velocity(speed, RPM)
+		self.motorLeft.spin(FORWARD)
+		
+	def driveRightMotor(self, speed):
+		self.motorRight.set_velocity(speed, RPM)
+		self.motorRight.spin(FORWARD)
 
-    # def wallFollowInches(self, setDistanceFromWall):
-    #     while frontRangeFinder.distance(DistanceUnits.IN) != setDistanceFromWall:
-    #         rightError = setDistanceFromWall - frontRangeFinder.distance(DistanceUnits.IN)
-    #         drive(-K_P * rightError)
 	
-	def spinAboutWheel(self, speed, deg, wheel, pause){
+	def spinAboutWheel(self, speed, deg, wheel, pause):
 		if wheel == "LEFT":
-			self.motorLeft.spin_for(FORWARD, deg * self.wheelRotDegPerBodyTurnDeg * (deg / abs(deg)), DEGREES, speed, RPM, pause)
-		else if wheel == "RIGHT":
-			self.motorRight.spin_for(FORWARD, deg * self.wheelRotDegPerBodyTurnDeg * (deg / abs(deg)), DEGREES, speed, RPM, pause)
-	}
+			self.motorRight.spin_for(REVERSE, self.finalDrive * deg * self.wheelRotDegPerBodyTurnDeg, DEGREES, speed, RPM, pause)
+		elif wheel == "RIGHT":
+			self.motorLeft.spin_for(REVERSE, self.finalDrive * deg * self.wheelRotDegPerBodyTurnDeg, DEGREES, speed, RPM, pause)
+
+	def spinInPlace(self, speed, deg, pause):
+		self.motorLeft.spin_for(FORWARD, self.finalDrive * deg / 2 * self.wheelRotDegPerBodyTurnDeg, DEGREES, speed, RPM, False)
+		self.motorRight.spin_for(FORWARD, - self.finalDrive * deg / 2 * self.wheelRotDegPerBodyTurnDeg, DEGREES, speed, RPM, pause)
+	
 	def brazeWallUntilDistance(self, rightFollowDist, forwardDistWall, speed, kp):
 		while self.frontRangeFinder.distance(DistanceUnits.IN) >= forwardDistWall:
 			rightError = rightFollowDist - self.rightRangeFinder.distance(DistanceUnits.IN)
@@ -138,25 +103,6 @@ class RBEDrivetrain:
 	#         rightError = setDistanceFromWall - frontRangeFinder.distance(DistanceUnits.IN)
 	#         drive(-K_P * rightError)
 
-    def driveDistance(self, Inches: float):
-        """Function to drive BaseBot straight for some number of inches"""
-        Velocity = 100
-        self.motorLeft.spin_for(
-            FORWARD,
-            Inches * self.finalDrive * self.rotationsPerInch,
-            RotationUnits.REV,
-            Velocity,
-            RPM,
-            False,
-        )
-        self.motorRight.spin_for(
-            FORWARD,
-            Inches * self.finalDrive * self.rotationsPerInch,
-            RotationUnits.REV,
-            Velocity,
-            RPM,
-            True,
-        )
 	# def driveDistance(self, Inches: float):
 	# 	"""Function to drive BaseBot straight for some number of inches"""
 	# 	Velocity = 100
@@ -177,25 +123,6 @@ class RBEDrivetrain:
 	# 		True,
 	# 	)
 
-    def turnInPlace(self, Rotations: float):
-        """Function to turn BaseBot for some number of Rotations"""
-        Velocity = 100
-        self.motorLeft.spin_for(
-            REVERSE,
-            Rotations * self.finalDrive * self.wheelTrack / self.wheelDiameter,
-            RotationUnits.REV,
-            Velocity,
-            RPM,
-            False,
-        )
-        self.motorRight.spin_for(
-            FORWARD,
-            Rotations * self.finalDrive * self.wheelTrack / self.wheelDiameter,
-            RotationUnits.REV,
-            Velocity,
-            RPM,
-            True,
-        )
 	# def turnInPlace(self, Rotations: float):
 	# 	"""Function to turn BaseBot for some number of Rotations"""
 	# 	Velocity = 100
@@ -216,30 +143,6 @@ class RBEDrivetrain:
 	# 		True,
 	# 	)
 
-    def turnAroundWheel(self, Rotations: float):
-        Velocity = 100
-        if Rotations > 0:
-            self.motorLeft.spin_for(
-                FORWARD,
-                Rotations * self.finalDrive * self.wheelTrack / self.wheelDiameter * 2,
-                RotationUnits.REV,
-                Velocity,
-                RPM,
-                True,
-            )
-        else:
-            self.motorRight.spin_for(
-                REVERSE,
-                Rotations * finalDrive * self.wheelTrack / self.wheelDiameter * 2,
-                RotationUnits.REV,
-                Velocity,
-                RPM,
-                True,
-            )
-
-
-# Functions
-brain = Brain()
 	# def turnAroundWheel(self, Rotations: float):
 	# 	Velocity = 100
 	# 	if Rotations > 0:
@@ -268,36 +171,31 @@ rightMotor = Motor(Ports.PORT10, 18_1, True)
 rangeFinderFront = Sonar(brain.three_wire_port.e)
 rangeFinderRight = Sonar(brain.three_wire_port.c)
 
-# conts
-kp = float(12)
 finalDrive = 5
 wheelDiameter = 4
 wheelBase = 8
 wheelTrack = 10.25
 
 rightFollowDistance = float(4.3)
+
 forwardWallDistance = float(5)
+
 forwardWallDistance2 = float(51)
 
 
-rbeDriveTrain = RBEDrivetrain(
-    rangeFinderFront,
-    rangeFinderRight,
-    leftMotor,
-    rightMotor,
-    finalDrive,
-    wheelDiameter,
-    wheelBase,
-    wheelTrack,
-)
-
+rbeDriveTrain = RBEDrivetrain(rangeFinderFront, rangeFinderRight, leftMotor, rightMotor, finalDrive, wheelDiameter, wheelBase, wheelTrack)
+							  
 brain.screen.print("running /n")
 
-rangeFinderFront.distance(DistanceUnits.IN)
+rangeFinderFront.distance(DistanceUnits.IN)     
 rangeFinderRight.distance(DistanceUnits.IN)
 
 wait(500)
 
-# line follow test
-rbeDriveTrain.brazeWallUntilDistance(rightFollowDistance, forwardWallDistance, 100, kp)
+#rbeDriveTrain.brazeWallUntilDistance(rightFollowDistance, forwardWallDistance, 100, kp)
+rbeDriveTrain.spinAboutWheel(100, 90, "RIGHT", True)
+rbeDriveTrain.spinAboutWheel(100, -90, "RIGHT", True)
+
+rbeDriveTrain.spinInPlace(100, 90, True)
 brain.screen.print("stopping /n")
+
