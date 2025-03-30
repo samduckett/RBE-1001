9f12baa7f312710b6daead31d91258455dca073e
bishopgov <brutalsysten.mail@gmail.com>
2025-03-30 18:06:18 -0400

    Gyro_Port Fix

--- Lab 2/Main.py
+++ Lab 2/Main.py
@@ -55,16 +55,20 @@ class RBEDrivetrain:
         if will use the gyro to true with more accuracy
         """
         if useGyro:
-            kP = 0.5
-            goalHeading = self.gyro.rotation() + deg
+            kP = .5
+            goalHeading = self.gyro.heading() + deg
 
             error = 999
             while abs(error) > 3:  # error less than 3 degrees\
-                error = goalHeading - self.gyro.rotation()
-                self.motorLeft.spin(FORWARD, speed + (kP * error), RPM)
-                self.motorRight.spin(FORWARD, -(speed + kP * error), RPM)
-                brain.screen.print_at("rotation", self.gyro.rotation(), x=40, y=90)
-                brain.screen.print_at("error", error, x=40, y=50)
+                error = goalHeading - self.gyro.heading()
+                self.motorLeft.spin(FORWARD, speed + kP * error, RPM)
+                self.motorRight.spin(FORWARD, speed - kP * error, RPM)
+                brain.screen.print_at(
+					"HEADING", self.gyro.heading(), x=40, y=90
+				)
+                brain.screen.print_at(
+					"error", error, x=40, y=50
+				)
             self.motorLeft.stop(HOLD)
             self.motorRight.stop(HOLD)
         else:
@@ -82,7 +86,7 @@ class RBEDrivetrain:
             )
             self.motorRight.spin_for(
                 FORWARD,
-                -motorSpinFor * (1 - rotationCOE),
+                - motorSpinFor * (1 - rotationCOE),
                 DEGREES,
                 speed,
                 RPM,
@@ -96,10 +100,10 @@ class RBEDrivetrain:
 
     def driveForwardDist(self, dist, speed, pause):
         self.motorLeft.spin_for(
-            FORWARD, dist / self.wheelCircumference, TURNS, speed, RPM, False
+            FORWARD, self.driveGearRatio * dist / self.wheelCircumference, TURNS, speed, RPM, False
         )
         self.motorRight.spin_for(
-            FORWARD, dist / self.wheelCircumference, TURNS, speed, RPM, pause
+            FORWARD, self.driveGearRatio * dist / self.wheelCircumference, TURNS, speed, RPM, pause
         )
 
     # Drive both motors at a set speed
@@ -175,10 +179,10 @@ armMotor = Motor(Ports.PORT8, 18_1, True)
 rangeFinderFront = Sonar(brain.three_wire_port.e)
 rangeFinderRight = Sonar(brain.three_wire_port.c)
 
-imu = Inertial(6)
+imu = Inertial(Ports.PORT6)
 
 # vars
-kp = float(12)
+kp = float(5)
 
 rightFollowDistance = float(4.3)
 forwardWallDistance = float(5)
@@ -211,18 +215,18 @@ arm = Arm(armMotor)
 
 def part1():
     rbeDriveTrain.driveForwardUntilDistance(3, 200)
-    # rbeDriveTrain.driveForwardDist(-5.5, 200, True)
+    #rbeDriveTrain.driveForwardDist(-5.5, 200, True)
     rbeDriveTrain.spin(100, 90)
-    rbeDriveTrain.brazeWallForDistane(5, 38, 100, kp)
+    rbeDriveTrain.brazeWallForDistane(5, 32, 100, kp)
     rbeDriveTrain.spin(100, 90)
-    rbeDriveTrain.driveForwardUntilDistance(40, 200)
+    rbeDriveTrain.driveForwardDist(16, 200, False)
 
     brain.screen.print("Stopping /n")
 
 
 def part2():
     # spins 90 deg
-    rbeDriveTrain.spin(150, 90, 0, True, True)
+    rbeDriveTrain.spin(50, 90, 0, True, True)
     # # spins -90 degres around left wheal
     # rbeDriveTrain.spin(100, -90, 1)
     # # spinds 90 degrees aroudn right wheal
@@ -242,4 +246,4 @@ def part4():
 
 # ZERO HEADING FOE GYRO
 imu.set_heading(0, DEGREES)
-part1()
+part2()
