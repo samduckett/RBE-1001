b4b12ca6034563e736dd7f3cbe19359d0354fb3d
Sam Duckett <67984574+samduckett@users.noreply.github.com>
2025-04-09 15:40:06 -0400

    adding fruit size detection

--- mian.py
+++ mian.py
@@ -13,9 +13,9 @@ class Vision:
         self,
     ):
         # fruit color configs
-        greenFruit = Colordesc(1, 12, 167, 71, 20, 0.2)
-        orangeFruit = Colordesc(2, 222, 66, 67, 10, 0.2)
-        yellowFruit = Colordesc(3, 166, 114, 59, 15, 0.2)
+        self.greenFruit = Colordesc(1, 12, 167, 71, 20, 0.2)
+        self.orangeFruit = Colordesc(2, 222, 66, 67, 10, 0.2)
+        self.yellowFruit = Colordesc(3, 166, 114, 59, 15, 0.2)
 
         # camera const
         self.camWidth = 320
@@ -26,11 +26,47 @@ class Vision:
         self.degPerPixelHeight = self.camVertFOV / self.camHeight
 
         # vision config
-        self.aiVision = AiVision(Ports.PORT14, greenFruit, orangeFruit, yellowFruit)
+        self.aiVision = AiVision(
+            Ports.PORT14, self.greenFruit, self.orangeFruit, self.yellowFruit
+        )
+
+        # arrays of fruit
+        self.objects = []
 
     def objectDist(self, pixelWidth, ObjectWidthIn):
         angularWidth = self.degPerPixelWidth * pixelWidth
         return (ObjectWidthIn * 0.5) / math.tan(math.radians(angularWidth * 0.5))
+
+    def update(self):
+        for obj in self.aiVision.take_snapshot(self.greenFruit):
+            self.objects.append(self.makeFruit(obj))
+
+    def makeFruit(self, obj):
+        fruit = Fruit()
+        fruit.originX = obj.originX
+        fruit.originY = obj.originY
+        fruit.centerX = obj.centerX
+        fruit.centerY = obj.centerY
+        fruit.width = obj.width
+        fruit.height = obj.height
+        fruit.score = obj.score
+        fruit.fruitColor = "green"
+        fruit.widthHeightRatio = fruit.width / fruit.height
+
+        return fruit
+
+
+class Fruit:
+    def __init__(self):
+        self.originX = 0
+        self.originY = 0
+        self.centerX = 0
+        self.centerY = 0
+        self.width = 0
+        self.height = 0
+        self.score = 0
+        self.fruitColor = ""
+        self.widthHeightRatio = 0
 
 
 class PID:
