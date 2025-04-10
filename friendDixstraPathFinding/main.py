9093b2e2d600f86d9d098f60b89c1a8789f99c80
Sam Duckett <67984574+samduckett@users.noreply.github.com>
2025-04-10 12:45:56 -0400

    adding pathfinding

--- friendDixstraPathFinding/main.py
+++ friendDixstraPathFinding/main.py
@@ -1,21 +1,102 @@
-from dijkstra import Dijkstra
-from navagation import Navigation
-from linefollow import Linefollow
+class GridNode:
+    def __init__(self, row, col):
+        self.row: int = row
+        self.col: int = col
+        self.dist: int = 100
+        self.previous: GridNode = None
+        self.neighbors: list[GridNode] = []
+        self.blocked: bool = False
+
+
+class Grid:
+    def __init__(self, rows: int, cols: int, blocked: list[tuple[int]]):
+        # initiate a dijkstra object with grid size and blocked intersections
+        self.rows: int = rows
+        self.cols: int = cols
+        self.current: tuple[int] = (0, 0)
+        self.grid: dict[tuple[int], GridNode] = {}  # creates a dictionary of nodes
+
+        for row in range(self.rows):
+            for col in range(self.cols):
+                self.grid[(row, col)] = GridNode(row, col)
+
+        for block in blocked:
+            self.grid[block].blocked = True
+
+        for n in self.grid.values():
+            node: GridNode = n
+            if node.row < self.rows - 1:
+                node.neighbors.append(self.grid[(node.row + 1, node.col)])
+            if node.row > 0:
+                node.neighbors.append(self.grid[(node.row - 1, node.col)])
+            if node.col < self.cols - 1:
+                node.neighbors.append(self.grid[(node.row, node.col + 1)])
+            if node.col > 0:
+                node.neighbors.append(self.grid[(node.row, node.col - 1)])
+
+    def get_node(self, loc: tuple[int]):
+        return self.grid[loc]
+
+    def compute_path(self, final):
+        # creates a queue setting all distances to 100 and the current to previous using dictionary
+        for node in self.grid:
+            self.grid[node].dist = 100
+        self.grid[self.current].dist = 0
+        self.grid[self.current].previous = None
+
+        startNode: GridNode = self.get_node(self.current)
+        queue: list[GridNode] = [startNode]
+
+        # sets distance between nodes and prior node
+        while queue:
+            currentNode = queue.pop(0)
+            if currentNode.blocked:
+                continue
+            for neighbor in currentNode.neighbors:
+                if neighbor.blocked:
+                    continue
+                newDist = currentNode.dist + 1
+                if newDist < neighbor.dist:
+                    neighbor.dist = newDist
+                    neighbor.previous = currentNode
+                    if neighbor not in queue:
+                        queue.append(neighbor)
+        # makes path end to start
+        path = []
+        finalNode = self.get_node(final)
+        while finalNode:
+            path.append((finalNode.row, finalNode.col))
+            finalNode = finalNode.previous
+
+        # flips and returns path
+        path.reverse()
+        self.current = final
+        return path
+
+    def print_grid(self):
+        spacing = 1
+        print("  " + " " * (spacing + 1), end="")
+        for col in range(self.cols):
+            print("\033[4m" + " " * spacing + f"{col}".zfill(2) + "\033[0m", end="")
+        for row in range(self.rows):
+            print("\n" + f"{row}".zfill(2) + " " * spacing + "|", end="")
+            for col in range(self.cols):
+                node = self.get_node((row, col))
+                print(
+                    " " * spacing + ("  " if node.blocked else f"{node.dist}".zfill(2)),
+                    end="",
+                )
+        print()
+
 
 # set desired stops and blocked nodes
-stops = [(4, 3), (3, 1), (2, 3), (0, 0)]
+# row col
+stops = [(4, 3), (0, 2)]
 blocked = [(4, 0), (3, 3), (2, 2), (0, 3)]
 # call Dijkstra and set grid size
-grid = Dijkstra(5, 10, blocked)
-# call navagation and set starting position
-nav = Navigation((0, 0))
-print('initializing')
+grid = Grid(20, 40, blocked)
 # loop for all stops in list
-for s in stops:
-    path = grid.compute_path(s)[1:]
-    print('Planned Path', path)
+for stop in stops:
+    path = grid.compute_path(stop)
+    print("Planned Path", path)
     grid.print_grid()
-    print(' ')
-    nav.navigate(path)
-print('completed')
-
--- mian.py
+++ mian.py
@@ -76,6 +76,97 @@ class PID:
         return abs(self.setpoint - measured_value) <= self.tolerance
 
 
+class GridNode:
+    def __init__(self, row, col):
+        self.row: int = row
+        self.col: int = col
+        self.dist: int = 100
+        self.previous: GridNode = None
+        self.neighbors: list[GridNode] = []
+        self.blocked: bool = False
+
+
+class Grid:
+    def __init__(self, rows: int, cols: int, blocked: list[tuple[int]]):
+        # initiate a dijkstra object with grid size and blocked intersections
+        self.rows: int = rows
+        self.cols: int = cols
+        self.current: tuple[int] = (0, 0)
+        self.grid: dict[tuple[int], GridNode] = {}  # creates a dictionary of nodes
+
+        for row in range(self.rows):
+            for col in range(self.cols):
+                self.grid[(row, col)] = GridNode(row, col)
+
+        for block in blocked:
+            self.grid[block].blocked = True
+
+        for n in self.grid.values():
+            node: GridNode = n
+            if node.row < self.rows - 1:
+                node.neighbors.append(self.grid[(node.row + 1, node.col)])
+            if node.row > 0:
+                node.neighbors.append(self.grid[(node.row - 1, node.col)])
+            if node.col < self.cols - 1:
+                node.neighbors.append(self.grid[(node.row, node.col + 1)])
+            if node.col > 0:
+                node.neighbors.append(self.grid[(node.row, node.col - 1)])
+
+    def get_node(self, loc: tuple[int]):
+        return self.grid[loc]
+
+    def compute_path(self, final):
+        # creates a queue setting all distances to 100 and the current to previous using dictionary
+        for node in self.grid:
+            self.grid[node].dist = 100
+        self.grid[self.current].dist = 0
+        self.grid[self.current].previous = None
+
+        startNode: GridNode = self.get_node(self.current)
+        queue: list[GridNode] = [startNode]
+
+        # sets distance between nodes and prior node
+        while queue:
+            currentNode = queue.pop(0)
+            if currentNode.blocked:
+                continue
+            for neighbor in currentNode.neighbors:
+                if neighbor.blocked:
+                    continue
+                newDist = currentNode.dist + 1
+                if newDist < neighbor.dist:
+                    neighbor.dist = newDist
+                    neighbor.previous = currentNode
+                    if neighbor not in queue:
+                        queue.append(neighbor)
+        # makes path end to start
+        path = []
+        finalNode = self.get_node(final)
+        while finalNode:
+            path.append((finalNode.row, finalNode.col))
+            finalNode = finalNode.previous
+
+        # flips and returns path
+        path.reverse()
+        self.current = final
+        return path
+
+    def print_grid(self):
+        spacing = 1
+        print("  " + " " * (spacing + 1), end="")
+        for col in range(self.cols):
+            print("\033[4m" + " " * spacing + f"{col}".zfill(2) + "\033[0m", end="")
+        for row in range(self.rows):
+            print("\n" + f"{row}".zfill(2) + " " * spacing + "|", end="")
+            for col in range(self.cols):
+                node = self.get_node((row, col))
+                print(
+                    " " * spacing + ("  " if node.blocked else f"{node.dist}".zfill(2)),
+                    end="",
+                )
+        print()
+
+
 class HDrive:
     def __init__(
         self,
